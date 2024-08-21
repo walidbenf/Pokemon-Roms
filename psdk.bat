2fb040ef0d718114040c52aa3e5180593a52a514
walidbenf <45196746+walidbenf@users.noreply.github.com>
2024-08-21 20:13:57 +0200

    Delete psdk.bat

--- psdk.bat
+++
@@ -1,4 +0,0 @@
-@@ECHO OFF
-CHCP 65001
-set PSDK_BINARY_PATH=C:\Users\Isior\AppData\Local\Programs\pokemon-studio\resources\psdk-binaries\
-"%PSDK_BINARY_PATH%ruby.exe" --disable=gems,rubyopt,did_you_mean Game.rb %*
