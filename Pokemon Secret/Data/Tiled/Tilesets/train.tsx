7cbf9ec8bfb093fb3456a2086fb9a842a33e9c76
Babok <wben-far@student.42.fr>
2024-08-26 01:29:58 +0200

    ras bg

---
+++ Pokemon Secret/Data/Tiled/Tilesets/train.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.0" name="train" tilewidth="32" tileheight="32" tilecount="42" columns="7">
+ <image source="../Assets/train.png" width="224" height="192"/>
+</tileset>
